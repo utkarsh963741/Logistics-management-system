aec5b559d8774c06818c4a3ab8e06ab4d70bb40a
Vineeth Madhu <vineethnbr13@gmail.com>
2022-01-12 01:13:50 +0530

    table added

--- pages/listLocation/table.js
+++ pages/listLocation/table.js
@@ -1,43 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout'
 import styles from '../../styles/Form.module.css'
-import 'react-tabulator/lib/styles.css';
-import { ReactTabulator } from 'react-tabulator'
 import { supabase } from '../../utils/supabaseClient'
 import { useRouter } from 'next/router'
+import Table from '../../components/Table'
+
+
+
+
 
 function table() {
+
+    const [tableData,setTableData]=useState(null)
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    
     const columns = [
-        { title: "Name", field: "name", width: 150 },
-        { title: "Age", field: "age", align: "left", formatter: "progress" },
-        { title: "Favourite Color", field: "col" },
-        { title: "Date Of Birth", field: "dob", align: "center" },
-        { title: "Rating", field: "rating", align: "center", formatter: "star" },
-        { title: "Passed?", field: "passed", align: "center", formatter: "tickCross" }
+        { title: 'EID', field: 'eid' },
+        { title: 'Type', field: 'type' },
+        { title: 'Latitude', field: 'latitude', type: 'numeric' },
+        { title: 'Longitude', field: 'longitude', type: 'numeric' },
+        { title: 'Capacity', field: 'capacity', type: 'numeric' },
+        { title: 'Status', field: 'status' },
     ]
-    var data = [
-        { id: 1, name: "Oli Bob", age: "12", col: "red", dob: "" },
-        { id: 2, name: "Mary May", age: "1", col: "blue", dob: "14/05/1982" },
-        { id: 3, name: "Christine Lobowski", age: "42", col: "green", dob: "22/05/1982" },
-        { id: 4, name: "Brendon Philips", age: "125", col: "orange", dob: "01/08/1980" },
-        { id: 5, name: "Margret Marmajuke", age: "16", col: "yellow", dob: "31/01/1999" },
-    ];
+    const title = "Locations"
+
+    async function fetchData() {
+        try {
+            const { data, error } = await supabase
+                .from('establishment')
+                .select(`*`)
+            if(data){
+                setTableData(data)
+                console.log(data)
+
+            }
+        }
+        catch{
+            alert(error.message)
+        }
+    }
+
+    if(tableData)
     return (
         <div>
 
             <Layout>
-                <div className={styles.container}>
-                    <h2 style={{ margin: "10px" }}>List of Locations</h2>
-                    <ReactTabulator
-                        data={data}
-                        columns={columns}
-                        tooltips={true}
-                        layout={"fitData"}
-                    />
+                <div style={{ width: '80vw', maxHeight: 'var(--container-height)' }}>
+                    <Table columns={columns} data={tableData} title={title}></Table>
                 </div>
+
+            </Layout>
+        </div>
+    )
+    else
+    return(
+        <div>
+            <Layout>
+
             </Layout>
         </div>
     )
 }
+
 
 export default table
